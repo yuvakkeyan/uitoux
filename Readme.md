7cd9e69402a6751530cbe832334a32531736de15
Yuvakkeyan <mail2yuvakarthi@gmail.com>
2023-05-22 15:14:22 +0530

    removed unwanted command

---
+++ Readme.md
@@ -0,0 +1,23 @@
+Created CRUD operation for the following collections.
+_____________________________________________________
+
+user,
+product,
+address,
+cart,
+order
+
+MiddleWares:
+___________
+
+1. auth
+It is used to check whether the user is authenticated or not using JWT token.
+
+2. checkRole
+It is used to check whether the loggedin user is a admin, vendor or user.
+______________________________________________________________________________
+
+***used MVC(Model View Controller)
+***The Entry point of the project is src/index.js
+
+command to run the code: node src/index.js
--- src/index.js
+++ src/index.js
@@ -4,70 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 const app = express();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 const PORT = process.env.PORT;
 
-const Admin = require("./models/user");
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// app.post("/api/login", async (req, res) => {
-//   try {
-//     var user = await Users.findOne({
-//       email: req.body.email,
-//     });
-
-//     if (!user) {
-//       res.status(400).send({
-//         msg: "Invalid Credentials",
-//       });
-//       return;
-//     }
-//     var isMatch = await bcrypt.compare(req.body.password, user.password);
-
-//     if (!isMatch) {
-//       res.status(400).send({
-//         msg: "Invalid Credentials",
-//       });
-//       return;
-//     }
-
-//     var token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY, {
-//       expiresIn: "1h",
-//     });
-//     await user.save();
-
-//     res.status(200).send({
-//       msg: "Login Successfully",
-//       token,
-//     });
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).send({
-//       msg: "Unable to Login",
-//     });
-//   }
-// });
-
-// // User Logout
-// app.get("/api/logout/:id", async (req, res) => {
-//   try {
-//     await Users.findByIdAndUpdate(req.params.id, {
-//       token: "",
-//     });
-//     res.status(200).send({
-//       msg: "Logout successfully",
-//     });
-//   } catch (e) {
-//     res.status(500).send({
-//       msg: "Unable to logout",
-//       error: e,
-//     });
-//   }
-// });
 
 routes.apiRoute(app);
 
